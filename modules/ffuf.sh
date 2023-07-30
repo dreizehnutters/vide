d01a3e46e9bc460c0d6757a82aafd01ad57e7c4a
geb <reach.fmk@gmail.com>
2023-07-30 17:44:36 +0200

    extended fuzz surface

--- modules/ffuf.sh
+++ modules/ffuf.sh
@@ -1,10 +1,9 @@
 # ---= directory brute force =---
 printf "$OP$IN$(basename $BASH_SOURCE) dir brute force:$RST\n"
-echo $TARGETS_FILE
 for TARGET in $(grep -v "#" "$TARGETS_FILE" | sort -V); do
     parse $TARGET
-    l2 ""
-    $FFUF -u "$PROTO://$IP:$PORT/FUZZ" -w "$WORDLIST" -mc all -ac -c -t "$THREADS" -of html -o "$FFUF_DIR/$FILE_NAME.html"
+    l2 
+    $FFUF -X METHOD -u "$PROTO://$IP:$PORT/FUZZ" -w "$METHODS":METHOD,"$WORDLIST":FUZZ -ac -c -e .php -t "$THREADS" -of html -o "$FFUF_DIR/$FILE_NAME.html"
     unset {PROTO,IP,PORT,FILE_NAME,DO_SSL}
 done
 # epilog
