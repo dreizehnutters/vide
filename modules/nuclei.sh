0ae861ccd95018219be5fa75ef59bad7f93853bf
dreizehnutters <reach.fmk@gmail.com>
2024-09-24 16:25:06 +0200

    fixed flags

--- modules/nuclei.sh
+++ modules/nuclei.sh
@@ -3,7 +3,7 @@ printf "$OP$IN$(basename $BASH_SOURCE) scans:$RST\n"
 for TARGET in $(grep -v "#" "$TARGETS_FILE" | sort -V); do
     l2 "full template scan of${IFACE}"
     cmd="$NUCLEI -templates "$NUCLEI_TEMPLATES" -u "$TARGET"\
-            -ni -irr -stats -timeout 1 -nh -no-stdin"
+            -no-interactsh -irr -stats -concurrency 25 -bulk-size 1 -nmhe -timeout 5 -nh -ss host-spray -no-stdin"
     log $cmd
     $cmd
     unset cmd
